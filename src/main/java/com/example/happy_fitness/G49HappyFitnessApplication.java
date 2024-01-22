c842231318ab9fab33cc93c20e52437438095388
duongdd141100 <duongdd141100@gmail.com>
2024-01-22 10:08:38 +0700

    Update name of project

--- src/main/java/com/example/happy_fitness/G49HappyFitnessApplication.java
+++ src/main/java/com/example/happy_fitness/G49HappyFitnessApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.happy_fitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class G49Application {
+public class G49HappyFitnessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(G49Application.class, args);
+        SpringApplication.run(G49HappyFitnessApplication.class, args);
     }
 
 }
--- src/test/java/com/example/happy_fitness/G49ApplicationTests.java
+++ src/test/java/com/example/happy_fitness/G49ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.happy_fitness;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
