198ac69f6c8f8eff8d0d236ce42f2ac87a8ab432
duongdd141100 <duongdd141100@gmail.com>
2024-04-05 00:57:55 +0700

    Update get image

--- src/main/java/com/example/happy_fitness/controller/ImageController.java
+++ src/main/java/com/example/happy_fitness/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.example.happy_fitness.controller;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,9 @@ import java.nio.file.Paths;
 @RestController
 public class ImageController {
 
-    private final Path root = Paths.get("./image/product");
+    private final Path root = Paths.get("image/product");
 
-    @GetMapping("/image/{filename}")
+    @GetMapping(value = "/image/{filename}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     public ResponseEntity<Resource> getImage(@PathVariable String filename) throws MalformedURLException {
         Path file = root.resolve(filename);
         Resource resource = new UrlResource(file.toUri());
