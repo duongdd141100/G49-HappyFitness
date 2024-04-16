162e5020eb1d73e44f091be83601288530258b86
duongdd141100 <duongdd141100@gmail.com>
2024-04-16 16:31:21 +0700

    Update dto

--- src/main/java/com/example/happy_fitness/dto/ProductDto.java
+++ src/main/java/com/example/happy_fitness/dto/ProductDto.java
@@ -4,13 +4,13 @@ import lombok.Data;
 
 @Data
 public class ProductDto {
-    private Float productId;
-    private Float facilityProductId;
+    private Long productId;
+    private Long facilityProductId;
     private String code;
     private String name;
     private String imagePath;
     private String category;
-    private Float supplierId;
+    private Long supplierId;
     private String supplier;
     private Float price;
     private String facility;
